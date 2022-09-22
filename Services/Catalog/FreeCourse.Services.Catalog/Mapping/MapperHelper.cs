bc98efc3363a1305aff07a8913e544b0e4987dbe
Emir Gürbüz <emir.gurbuz@simbt.com.tr>
2022-09-22 11:55:22 +0300

    Create MapperHelper

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Mapping/MapperHelper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+
+namespace FreeCourse.Services.Catalog.Mapping;
+
+public class MapperHelper : Profile
+{
+    public MapperHelper()
+    {
+        CreateMap<Category, CategoryDto>().ReverseMap();
+
+        CreateMap<Course, CourseDto>().ReverseMap();
+        CreateMap<Course, CourseCreateDto>().ReverseMap();
+        CreateMap<Course, CourseUpdateDto>().ReverseMap();
+
+        CreateMap<Feature, FeatureDto>().ReverseMap();
+    }
+}
--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,3 +1,5 @@
+using FreeCourse.Services.Catalog.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+#region AutoMapper
+builder.Services.AddAutoMapper(typeof(CategoryDto));
+#endregion
+
 
 var app = builder.Build();
 
