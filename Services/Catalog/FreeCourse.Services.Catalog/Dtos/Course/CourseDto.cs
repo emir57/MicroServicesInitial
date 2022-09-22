26dce9530f1bdfd84f24e9439df4b58ccce05e91
Emir Gürbüz <emir.gurbuz@simbt.com.tr>
2022-09-22 11:49:51 +0300

    Create Dtos

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/Course/CourseDto.cs
@@ -0,0 +1,17 @@
+﻿using FreeCourse.Services.Catalog.Dtos.Category;
+
+namespace FreeCourse.Services.Catalog.Dtos;
+
+public class CourseDto
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+    public string UserId { get; set; }
+    public string Picture { get; set; }
+    public DateTime CreatedTime { get; set; }
+    public FeatureDto Feature { get; set; }
+    public string CateogoryId { get; set; }
+    public CategoryDto Category { get; set; }
+}
