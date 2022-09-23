9102deacff2b8b7a762dc5d768aa4136d54422c9
emir57 <emir.gurbuz06@hotmail.com>
2022-09-23 22:34:10 +0300

    UserController

---
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using FreeCourse.IdentityServer.Dtos;
+using FreeCourse.IdentityServer.Models;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using Mapster;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.IdentityServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : CustomBaseController
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup([FromBody] SignupDto signupDto)
+        {
+            ApplicationUser user = signupDto.Adapt<ApplicationUser>();
+
+            IdentityResult result = await _userManager.CreateAsync(user, signupDto.Password);
+            if (result.Succeeded == false)
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+
+            return NoContent();
+        }
+    }
+}
