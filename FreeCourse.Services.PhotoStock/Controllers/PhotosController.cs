a2e4a21ccf44534d2412076290f6713f7a1584f8
emir57 <emir.gurbuz@simbt.com.tr>
2022-09-23 13:50:18 +0300

    FileUpload

---
+++ FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PhotosController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+            var stream = new FileStream(path, FileMode.Create);
+
+            await photo.CopyToAsync(stream, cancellationToken);
+
+            var returnPath = $"photos/{photo.FileName}";
+
+            return Ok(returnPath);
+        }
+    }
+}
