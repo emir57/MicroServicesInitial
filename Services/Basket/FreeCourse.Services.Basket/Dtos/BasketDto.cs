fa0880e933a0d4b0f4755e4ffd5d15d0c2475ed8
emir57 <emir.gurbuz06@hotmail.com>
2022-09-24 12:39:19 +0300

    Create BasketDto And BasketItemDto

---
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FreeCourse.Services.Basket.Dtos;
+
+public sealed class BasketDto
+{
+    public string? UserId { get; set; }
+    public string? DiscountCode { get; set; }
+    public List<BasketItemDto> BasketItems { get; set; }
+
+    public decimal TotalPrice
+        => BasketItems.Sum(y => y.Price);
+}
---
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FreeCourse.Services.Basket.Dtos;
+
+public sealed class BasketItemDto
+{
+    public byte Quantity { get; set; }
+    public string? CourseId { get; set; }
+    public string? CourseName { get; set; }
+    public decimal Price { get; set; }
+}
