1f8b8f90bbafc59c4b662f0401601854be1e060d
emir57 <emir.gurbuz06@hotmail.com>
2022-09-24 12:48:55 +0300

    Create IBasketService

---
+++ Services/Basket/FreeCourse.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+
+namespace FreeCourse.Services.Basket.Services;
+
+public interface IBasketService
+{
+    Task<Response<BasketDto>> GetBasketAsync(string userId);
+    Task<Response<bool>> SaveOrUpdateAsync(BasketDto basketDto);
+    Task<Response<bool>> DeleteAsync(string userId);
+}
