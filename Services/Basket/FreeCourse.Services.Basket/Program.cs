62fd30c1368eb93c0b564476b522b2d051b715bb
emir57 <emir.gurbuz06@hotmail.com>
2022-09-24 12:46:24 +0300

    RedisSettings

--- Services/Basket/FreeCourse.Services.Basket/Program.cs
+++ Services/Basket/FreeCourse.Services.Basket/Program.cs
@@ -1,3 +1,5 @@
+using FreeCourse.Services.Basket.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Redis Settings
+builder.Services.AddSingleton(typeof(RedisSettings),
+    x => builder.Configuration.GetSection(nameof(RedisSettings)).Get<RedisSettings>());
+#endregion
 
 var app = builder.Build();
 
---
+++ Services/Basket/FreeCourse.Services.Basket/Settings/RedisSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace FreeCourse.Services.Basket.Settings;
+
+public sealed class RedisSettings
+{
+    public RedisSettings(string host, int port) : this()
+    {
+        Host = host;
+        Port = port;
+    }
+    public RedisSettings()
+    {
+
+    }
+    public string Host { get; set; }
+    public int Port { get; set; }
+}
