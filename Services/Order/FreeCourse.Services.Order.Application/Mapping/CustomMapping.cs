d0536d2f356e5c343999e98ef010cff96176a8ce
emir57 <emir.gurbuz@simbt.com.tr>
2022-09-26 10:25:19 +0300

    Create ObjectMapper And Mapper Profile

---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Services.Order.Domain.OrderAggregate;
+
+namespace FreeCourse.Services.Order.Application.Mapping;
+
+public class CustomMapping : Profile
+{
+    public CustomMapping()
+    {
+        CreateMap<OrderDto, Domain.OrderAggregate.Order>().ReverseMap();
+        CreateMap<AddressDto, Address>().ReverseMap();
+        CreateMap<OrderItemDto, OrderItem>().ReverseMap();
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace FreeCourse.Services.Order.Application.Mapping;
+
+public sealed class ObjectMapper
+{
+    public static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+    {
+        var config = new MapperConfiguration(configure =>
+        {
+            configure.AddProfile(new CustomMapping());
+        });
+
+        IMapper mapper = config.CreateMapper();
+        return mapper;
+    });
+}
