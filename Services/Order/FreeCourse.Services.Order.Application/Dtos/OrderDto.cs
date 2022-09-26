5a54a131d9492954bf30db54b66ed4506a1e4730
emir57 <emir.gurbuz@simbt.com.tr>
2022-09-26 10:19:29 +0300

    Create Dtos

---
+++ Services/Order/FreeCourse.Services.Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using FreeCourse.Services.Order.Domain.OrderAggregate;
+
+namespace FreeCourse.Services.Order.Application.Dtos;
+
+public class OrderDto
+{
+    public int Id { get; set; }
+    public DateTime CreatedDate { get; private set; }
+    public AddressDto Address { get; private set; }
+    public string BuyerId { get; private set; }
+    private List<OrderItemDto> OrderItems { get; set; }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Application/Dtos/OrderItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FreeCourse.Services.Order.Application.Dtos;
+
+public class OrderItemDto
+{
+    public int Id { get; set; }
+    public string ProductId { get; private set; }
+    public string ProductName { get; private set; }
+    public string PictureUrl { get; private set; }
+    public decimal Price { get; private set; }
+}
