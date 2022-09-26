20b0f13c080dbdcdf608d4cdb6ac00fb9f3f1626
emir57 <emir.gurbuz@simbt.com.tr>
2022-09-26 11:51:34 +0300

    ContextAccessor and SharedIdentity In Program.cs

---
+++ Services/Order/FreeCourse.Services.Order.API/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Service;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Order.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : CustomBaseController
+    {
+        private readonly IMediator _mediator;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public OrdersController(IMediator mediator, ISharedIdentityService sharedIdentityService)
+        {
+            _mediator = mediator;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+
+    }
+}
--- Services/Order/FreeCourse.Services.Order.API/Program.cs
+++ Services/Order/FreeCourse.Services.Order.API/Program.cs
@@ -1,5 +1,9 @@
+using FreeCourse.Services.Order.Application.Features.Commands.CreateOrder;
 using FreeCourse.Services.Order.Infrastructure;
+using FreeCourse.Shared.Service;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +14,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#region MediatR
+builder.Services.AddMediatR(typeof(CreateOrderCommand));
+#endregion
+
+#region ContextAccessor and SharedIdentity
+builder.Services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+
+builder.Services.AddHttpContextAccessor();
+#endregion
 
 #region AddDbContext
 builder.Services.AddDbContext<OrderDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"), configure =>
