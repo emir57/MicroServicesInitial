d37ef0d57d9b9547df57158d4d50894f4ba235f8
emir57 <emir.gurbuz@simbt.com.tr>
2022-09-28 15:46:48 +0300

    Refactoring DiscountService

---
+++ Services/Discount/FreeCourse.Services.Discount/Constants/DiscountServiceMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace FreeCourse.Services.Discount.Constants
+{
+    public static class DiscountServiceMessages
+    {
+        public static readonly string DeletedFail = "An error occurred while deleting";
+        public static readonly string DiscountIsNotFound = "Discount is not found";
+        public static readonly string AddedFail = "An error occurred while adding";
+        public static readonly string UpdatedFail = "An error occurred while updating";
+    }
+}
--- Services/Discount/FreeCourse.Services.Discount/Services/DiscountService.cs
+++ Services/Discount/FreeCourse.Services.Discount/Services/DiscountService.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using FreeCourse.Services.Discount.Constants;
 using FreeCourse.Shared.Dtos;
 using Npgsql;
 using System.Data;
@@ -21,7 +22,7 @@ public sealed class DiscountService : IDiscountService
         int deleteStatus = await _dbConnection.ExecuteAsync("delete from discount where id=@Id", new { id });
         if (deleteStatus > 0)
             return Response<NoContent>.Success(204);
-        return Response<NoContent>.Fail("An error occurred while deleting", 500);
+        return Response<NoContent>.Fail(DiscountServiceMessages.DeletedFail, 500);
     }
 
     public async Task<Response<List<Models.Discount>>> GetAllAsync()
@@ -35,7 +36,7 @@ public sealed class DiscountService : IDiscountService
         Models.Discount discount = await _dbConnection.QueryFirstOrDefaultAsync<Models.Discount>("select * from discount where code=@Code and userid=@UserId"
             , new { code, userId });
         if (discount == null)
-            return Response<Models.Discount>.Fail("Discount is not found", 404);
+            return Response<Models.Discount>.Fail(DiscountServiceMessages.DiscountIsNotFound, 404);
         return Response<Models.Discount>.Success(discount, 200);
     }
 
@@ -43,7 +44,7 @@ public sealed class DiscountService : IDiscountService
     {
         Models.Discount discount = await _dbConnection.QueryFirstOrDefaultAsync<Models.Discount>("select * from discount where id=@Id", new { id });
         if (discount == null)
-            return Response<Models.Discount>.Fail("Discount not found", 404);
+            return Response<Models.Discount>.Fail(DiscountServiceMessages.DiscountIsNotFound, 404);
         return Response<Models.Discount>.Success(discount, 200);
     }
 
@@ -52,7 +53,7 @@ public sealed class DiscountService : IDiscountService
         int saveStatus = await _dbConnection.ExecuteAsync("insert into discount(userid,rate,code)values(@UserId,@Rate,@Code)", discount);
         if (saveStatus > 0)
             return Response<NoContent>.Success(204);
-        return Response<NoContent>.Fail("An error occurred while adding", 500);
+        return Response<NoContent>.Fail(DiscountServiceMessages.AddedFail, 500);
     }
 
     public async Task<Response<NoContent>> UpdateAsync(Models.Discount discount)
@@ -60,6 +61,6 @@ public sealed class DiscountService : IDiscountService
         int updateStatus = await _dbConnection.ExecuteAsync("update discount set userid=@UserId,rate=@Rate,code=@Code where id=@Id", discount);
         if (updateStatus > 0)
             return Response<NoContent>.Success(204);
-        return Response<NoContent>.Fail("An error occurred while updating", 500);
+        return Response<NoContent>.Fail(DiscountServiceMessages.UpdatedFail, 500);
     }
 }
