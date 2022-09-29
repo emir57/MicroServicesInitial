4363d68bf0e11f97f10a553975fe0905e986e057
emir57 <emir.gurbuz06@hotmail.com>
2022-09-29 11:25:59 +0300

    ExceptionLogEventConsumer

--- Services/LogAPI/FreeCourse.Services.LogAPI/Consumers/ExceptionLogEventConsumer.cs
+++ Services/LogAPI/FreeCourse.Services.LogAPI/Consumers/ExceptionLogEventConsumer.cs
@@ -1,12 +1,25 @@
-﻿using FreeCourse.Shared.Messages;
+﻿using FreeCourse.Shared.CrossCuttingConcerns.Serilog;
+using FreeCourse.Shared.Messages;
 using MassTransit;
+using System.Text.Json;
 
 namespace FreeCourse.Services.LogAPI.Consumers;
 
 public class ExceptionLogEventConsumer : IConsumer<ExceptionLogEvent>
 {
+    private readonly LoggerServiceBase _loggerServiceBase;
+
+    public ExceptionLogEventConsumer(LoggerServiceBase loggerServiceBase)
+    {
+        _loggerServiceBase = loggerServiceBase;
+    }
+
     public Task Consume(ConsumeContext<ExceptionLogEvent> context)
     {
-        throw new NotImplementedException();
+        return Task.Run(() =>
+        {
+            string logMessage = JsonSerializer.Serialize(context.Message);
+            _loggerServiceBase.Error(logMessage);
+        });
     }
 }
