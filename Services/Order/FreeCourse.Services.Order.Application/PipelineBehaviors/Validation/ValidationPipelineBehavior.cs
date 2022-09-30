957a7940135ed02994bbc8867cdd62986467c5db
emir57 <emir.gurbuz06@hotmail.com>
2022-09-30 09:08:03 +0300

    Create ValidationPipelineBehavior

---
+++ Services/Order/FreeCourse.Services.Order.Application/PipelineBehaviors/Validation/ValidationPipelineBehavior.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace FreeCourse.Services.Order.Application.PipelineBehaviors.Validation;
+
+public class ValidationPipelineBehavior<TRequest, TResponse> : BasePipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    //private readonly
+
+    protected override void OnBefore(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+        
+    }
+}
